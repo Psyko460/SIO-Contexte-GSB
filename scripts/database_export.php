e1e4a7257514f19be34fa43160a98d06bf4a5d0a
Grinz <solenemarie67@gmail.com>
2016-09-23 16:24:51 +0200

    scripts

--- scripts/database_export.php
+++ scripts/database_export.php
@@ -5,6 +5,7 @@
   try
   {
     $output = shell_exec("mysqldump --opt --default-character-set=UTF8 --single-transaction --protocol=TCP -u --user=root --password='' --host=localhost gsb_frais > backupDatabase.sql");
+    echo $output;
 
     echo("Base de données correctement sauvegardée !");
   }
