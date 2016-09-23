bc5f04c42e36823873df3ec774321d3b3c72a80b
Grinz <solenemarie67@gmail.com>
2016-09-23 19:45:44 +0200

    scripts

---
+++ scripts/database_restore.php
@@ -0,0 +1,14 @@
+<?php
+
+  try
+  {
+    $output = shell_exec(`mysql --user=root --password=root < /var/www/GSBProject/scripts/backupDatabase.sql`);
+
+    echo('Base de données correctement importée !');
+  }
+  catch (Exception $e)
+  {
+    die('Erreur : ' . $e->getMessage());
+  }
+
+?>
