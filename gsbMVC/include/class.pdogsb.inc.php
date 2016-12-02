e07aad0754424b52ac66384a3c78343532346c4e
root <root@ubuntu-ak.(none)>
2016-12-02 14:25:54 +0100

    Mise a jour 02.12.2016

--- gsbMVC/include/class.pdogsb.inc.php
+++ gsbMVC/include/class.pdogsb.inc.php
@@ -19,7 +19,7 @@ class PdoGsb{
       	private static $serveur='mysql:host=localhost';
       	private static $bdd='dbname=gsb_frais';
       	private static $user='root' ;
-      	private static $mdp='ArtemisLL' ;
+      	private static $mdp='ArtemisLL';
 		private static $monPdo;
 		private static $monPdoGsb=null;
 /**
