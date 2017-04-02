91060d6c02d1f9940c71d11a5d3728b917953c86
Grinz <solenemarie67@gmail.com>
2017-04-02 14:18:07 +0200

    Footer informations

--- gsbMVC/vues/v_pied.php
+++ gsbMVC/vues/v_pied.php
@@ -5,23 +5,22 @@
             <div class="row">
                <div class="col l6 s12">
                   <img src="./images/gsb_logo.png" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
-                  <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                  <p class="grey-text text-lighten-4">Laboratoire Galaxy Swiss Bourdin</p>
                </div>
             <div class="col l4 offset-l2 s12">
-               <h5 class="white-text">Links</h5>
+               <h5 class="white-text">Contact</h5>
                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li class="grey-text text-lighten-3">Lycée René Cassin</li>
+                  <li class="grey-text text-lighten-3">4 Rue Schoch - BP 67</li>
+                  <li class="grey-text text-lighten-3">67046 Strasbourg CEDEX</li>
+                  <li class="grey-text text-lighten-3">03 88 45 54 54</li>
                </ul>
             </div>
             </div>
          </div>
          <div class="footer-copyright">
             <div class="container">
-               © 2014 Copyright Text
-               <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+               © 2017 BTS SIO SLAM - Lycée René Cassin
             </div>
          </div>
       </footer>
